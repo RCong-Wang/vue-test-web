4f9fb3716d5a7d886e52e1f275d1b2a74c209508
rcwang <jinnig177773@gmail.com>
2024-01-12 23:08:53 +0800

    Update GitHub Pages files

--- src/main.js
+++ src/main.js
@@ -11,8 +11,8 @@ import axiosInstance from './api/index'; // 导入 axios 实例
 const fix_data = {
     data() {
         return {
-            local_http: 'https://b4a8-2401-e180-8850-ae-c03b-534a-dcee-f333.ngrok-free.app',
-            local_ws:'wss://b4a8-2401-e180-8850-ae-c03b-534a-dcee-f333.ngrok-free.app'
+            local_http: 'https://aa0e-2401-e180-8850-ae-c03b-534a-dcee-f333.ngrok-free.app',
+            local_ws:'wss://aa0e-2401-e180-8850-ae-c03b-534a-dcee-f333.ngrok-free.app'
         };
     },
 };
