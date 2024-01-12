86e1fad9fd2a3db41001101514169ea0bb472855
rcwang <jinnig177773@gmail.com>
2024-01-12 15:00:52 +0800

    Fix GitHub Pages configuration

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,8 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
-  publicPath: './',
+  // publicPath: './',
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue-test-web/' : '/',
   devServer:{
     historyApiFallback: true,
     allowedHosts: "all",
